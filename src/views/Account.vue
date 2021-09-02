77434522beeb933fc4a14832a05eccb0252ad44e
shensven <shensven2016@gmail.com>
2021-09-02 22:54:43 +0800

    fix: minor UI update

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -3,18 +3,22 @@
     <v-container>
       <v-row>
         <v-col class="mt-8 ml-4 mr-4">
-          <v-text-field label="username" dense></v-text-field>
-          <v-text-field label="password" dense class="mt-4"></v-text-field>
-          <v-btn large depressed width="100%" class="mt-8">Login</v-btn>
+          <v-text-field label="用户名" solo></v-text-field>
+          <v-text-field label="密码" solo></v-text-field>
+          <v-btn
+            width="100%"
+            class="mt-8"
+            large
+            depressed
+            :loading="loading"
+            :disabled="loading"
+            @click="loader = 'loading'"
+            >登录</v-btn
+          >
 
           <v-checkbox
-            label="Remember password"
+            label="记住密码"
             v-model="rememberPassword"
-            hide-details
-          ></v-checkbox>
-          <v-checkbox
-            label="Auto Login"
-            v-model="autoLogin"
             hide-details
           ></v-checkbox>
         </v-col>
@@ -28,7 +32,17 @@ export default {
   name: 'Account',
   data: () => ({
     rememberPassword: false,
-    autoLogin: false
-  })
+    loading: false
+  }),
+  watch: {
+    loader() {
+      const l = this.loader
+      this[l] = !this[l]
+
+      setTimeout(() => (this[l] = false), 3000)
+
+      this.loader = null
+    }
+  }
 }
 </script>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,5 +1,74 @@
 <template>
-  <div class="about">
-    <h1 class="user-select-none">This is an settings page</h1>
-  </div>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col class="mt-8 ml-4 mr-4">
+          <v-text-field
+            label="网关"
+            solo
+            persistent-hint
+            :hint="hintGateway"
+            v-model="gateway"
+            @keyup.enter="persist"
+          ></v-text-field>
+          <v-text-field
+            label="物理地址"
+            solo
+            persistent-hint
+            :hint="hintMac"
+            v-model="mac"
+            @keyup.enter="persist"
+          ></v-text-field>
+          <v-btn width="100%" class="mt-8" large depressed @click="persist"
+            >保存</v-btn
+          >
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
+
+<script>
+export default {
+  name: 'Settings',
+  data: () => ({
+    gateway: '',
+    mac: '',
+    hintGateway: '',
+    hintMac: ''
+  }),
+  mounted() {
+    if (localStorage.gateway) {
+      this.hintGateway = localStorage.gateway
+    }
+    if (localStorage.mac) {
+      this.hintMac = localStorage.mac
+    }
+  },
+  watch: {
+    // gateway(newGateway) {
+    //   localStorage.gateway = newGateway
+    // }
+  },
+  methods: {
+    persist() {
+      if (this.gateway.length > 0 && this.mac.length > 0) {
+        localStorage.gateway = this.gateway
+        localStorage.mac = this.mac
+        this.hintGateway = this.gateway
+        this.hintMac = this.mac
+        this.gateway = ''
+        this.mac = ''
+      } else if (this.gateway.length > 0) {
+        localStorage.gateway = this.gateway
+        this.hintGateway = this.gateway
+        this.gateway = ''
+      } else if (this.mac.length > 0) {
+        localStorage.mac = this.mac
+        this.hintMac = this.mac
+        this.mac = ''
+      }
+    }
+  }
+}
+</script>
